32cc1a2a70fc203a9e1869faac3a54ddcae5a919
Mateo <msteer05@hotmail.com>
2021-08-29 00:43:17 -0500

    Feature 1.
    Commit del feature 1

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "student.h"
 
 int main(int argc, char *argv[]){
 
-    return EXIT_SUCCESS;
+    FILE *inFile = fopen("./fin","r");
+    FILE *inFileOut = fopen("./fout","a+");
+    if(inFile == NULL){
+        perror("Open File fin Fails :");
+        return(EXIT_FAILURE);
+    }
+    if(inFileOut == NULL){
+        perror("Open File fout fails: ");
+        return(EXIT_FAILURE);
+    }
+    for(int i=0;i<8;i++){
+        if(i ==1 ) feature1(inFile,inFileOut);
+    }
+    
+    fclose(inFile);
+    fclose(inFileOut);
 }
