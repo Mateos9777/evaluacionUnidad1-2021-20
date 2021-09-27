eaf558819b7cb94a89503f1c5f816412b2c09038
Mateo <msteer05@hotmail.com>
2021-09-27 14:18:34 -0500

    Feature4
    Se corrigen algunas cosas del feature 3 y se anexa feature 4 con

--- student.c
+++ student.c
@@ -36,17 +36,17 @@ void feature2(FILE *fin, FILE *fout)
 {
     char* bufer = (char *)calloc(tamano_maximo,sizeof(char));
     fgets(bufer,tamano_maximo,fin);
-    char* temp = (char *)calloc(1,sizeof(char));    
+    char* temporal = (char *)calloc(1,sizeof(char));    
     char* bufer2 = strtok(bufer,"\n");
     int lon = strlen(bufer2);
     for(int i=0;i<(lon/2);i++){//ciclo para Leer al reves el texto siendo el indice i
-        *temp = bufer2[i];
+        *temporal = bufer2[i];
         bufer2[i] = bufer2[lon - i - 1];
-        bufer2[lon-i - 1 ] = *temp;
+        bufer2[lon-i - 1 ] = *temporal;
     }
     fprintf(fout,"%s\n",bufer2);
     free(bufer2);
-    free(temp);
+    free(temporal);
 }
 
 //lee el arreglo de enteros de la tercera línea del archivo de entrada,
@@ -54,27 +54,60 @@ void feature2(FILE *fin, FILE *fout)
 //calcula la suma y almacena el resultado en la tercera línea del archivo de salida.
 void feature3(FILE *fin, FILE *fout){
 
-char* bufer = (char *)calloc(tamano_maximo,sizeof(char));
-    short* numeros = (short *) calloc(tamano_maximo,sizeof(short));
-    char* temp = (char *)calloc(4,sizeof(char));
+char* bufer = (char *)calloc(tamano_maximo,sizeof(char));// uso de calloc para reservar
+    short* numeros = (short *) calloc(tamano_maximo,sizeof(short));//uso calloc para reservar
+    char* temporal = (char *)calloc(4,sizeof(char));
     char *bash;
     fgets(bufer,tamano_maximo,fin);
     int contador=0;
     int suma = 0;
-    temp = strtok(bufer, " ");
-    while(temp != NULL){
-        numeros[contador] = strtol(temp,&bash,10);
+    temporal = strtok(bufer, " ");
+    while(temporal != NULL){
+        numeros[contador] = strtol(temporal,&bash,10);
         suma = suma + numeros[contador];
-        temp = strtok(NULL, " ");
+        temporal = strtok(NULL, " ");
         contador++;        
     }
     fprintf(fout,"%d\n",suma);
     free(bufer);    
-    free(temp);
+    free(temporal);
     free(numeros);
 }
-void feature4(FILE *fin, int **parr, int *length, char **op)
-{
+
+
+//lee el arreglo de enteros de la cuarta línea del archivo de 
+//entrada así como la operación especificada luego del arreglo, 
+//separada por un espacio.
+//Aca igualmente se usa la operacion de calloc para reservar la memoria que vamos a necesitar
+//Se usan comentarios con printf para ir verificando que si entre en las instruciones
+//ya que no empleamos el archivo de salida para verificar.
+void feature4(FILE *fin, int **parr, int *length, char **op){
+    char* bufer = (char *)calloc(tamano_maximo,sizeof(char));
+    char* temporal = (char *)calloc(4,sizeof(char));
+    char *bash = (char *)calloc(4,sizeof(char));
+    *parr = (int *)calloc(tamano_maximo,sizeof(int));
+    *op = (char *)calloc(4,sizeof(char));
+    *length = 0;
+    int cont=0;
+    fgets(bufer,tamano_maximo,fin);
+    temporal = strtok(bufer, " ");
+    
+    while(temporal != NULL){
+        (*parr)[cont] = strtol(temporal,&bash,10);
+        temporal = strtok(NULL, " ");
+        cont++;
+        //printf("ENTRA AL CICLO CORRECTAMENTE\n");
+        //printf("%c\n",*bufer);
+        
+    }
+    //printf("sale del ciclo\n");
+    strncpy(*op,bash,3);
+    *length = cont;
+    free(bufer);
+    free(temporal);
+    //free(parr); Se comenta porque ahora lo volvemos a usar
+
+
 }
 void feature5(FILE *fout, int *parr, int length, char *op)
 {
